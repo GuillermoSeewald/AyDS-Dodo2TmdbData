a9697fcf9986c8627e33e2eedc72fa0f4216c8ed
Romina <romivire@hotmail.com>
2020-07-07 16:29:45 -0300

    Renamed ExternalService

--- src/main/java/ayds/dodo2/tmdb/TmdbService.kt
+++ src/main/java/ayds/dodo2/tmdb/TmdbService.kt
@@ -2,6 +2,6 @@ package ayds.dodo2.tmdb
 
 import ayds.dodo2.tmdb.entities.TmdbMovie
 
-interface ExternalService {
+interface TmdbService {
     fun getMovieInfo(movieTitle: String, movieYear:String): TmdbMovie
 }
